088b3f8599bfbe5e7e4fb60ceab0e630cf7c89bc
Mason Drabik <mkdrabik@unc.edu>
2025-04-06 16:50:41 -0400

    commit

--- frontend/src/app/coworking/git-together/initial-form/initial-form.component.ts
+++ frontend/src/app/coworking/git-together/initial-form/initial-form.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { IFService } from './initial-form.service';
-import { Profile, ProfileService } from 'src/app/profile/profile.service';
-import { ActivatedRoute } from '@angular/router';
-import { profileResolver } from 'src/app/profile/profile.resolver';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -16,34 +11,23 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class InitialFormComponent {
   form: FormGroup;
   public static Route = {
-    path: 'initialForm',
-    title: 'Initial Form',
-    component: InitialFormComponent,
-    resolve: {
-      profile: profileResolver
-    }
+    path: 'initial-form',
+    title: 'Git Together',
+    component: InitialFormComponent
   };
-  profile: Profile;
 
   constructor(
     private fb: FormBuilder,
-    private ifservice: IFService,
-    private route: ActivatedRoute,
     private router: Router,
     private snackBar: MatSnackBar
   ) {
     this.form = this.fb.group({
-      one: [1, Validators.required],
-      two: [1, Validators.required],
-      three: [1, Validators.required],
-      four: [1, Validators.required],
-      five: [1, Validators.required]
+      deadlineProximity: [3, Validators.required],
+      workStyle: [3, Validators.required],
+      leadershipComfort: [3, Validators.required],
+      meetingFrequency: [3, Validators.required],
+      conflictResolution: [3, Validators.required]
     });
-
-    const data = this.route.snapshot.data as {
-      profile: Profile;
-    };
-    this.profile = data.profile;
   }
 
   navigateToGitTogether() {
@@ -51,25 +35,16 @@ export class InitialFormComponent {
   }
 
   onSubmit() {
-    console.log(this.form.value);
-    this.ifservice.generate_answers(
-      this.form.value.one,
-      this.form.value.two,
-      this.form.value.three,
-      this.form.value.four,
-      this.form.value.five,
-      this.profile.pid
-    );
     if (this.form.valid) {
       // Enhanced logging with formatted output
       const formValues = this.form.value;
       const logMessage = `
         Initial Preferences Submitted:
-        - Deadline Proximity: ${formValues.one}/5
-        - Work Style: ${formValues.two}/5
-        - Leadership Comfort: ${formValues.three}/5
-        - Meeting Frequency: ${formValues.four}/5
-        - Conflict Resolution: ${formValues.five}/5
+        - Deadline Proximity: ${formValues.deadlineProximity}/5
+        - Work Style: ${formValues.workStyle}/5
+        - Leadership Comfort: ${formValues.leadershipComfort}/5
+        - Meeting Frequency: ${formValues.meetingFrequency}/5
+        - Conflict Resolution: ${formValues.conflictResolution}/5
       `;
 
       console.log('Form values:', formValues);
